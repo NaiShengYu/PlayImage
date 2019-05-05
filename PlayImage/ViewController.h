e30971f823f3235c3c1f1eefd728a1fe6ad22d9c
NaiShengYu <2495024529@qq.com>
2019-05-05 13:27:44 +0800

    Initial Commit

---
+++ PlayImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayImage
+//
+//  Created by Nasheng Yu on 2019/5/5.
+//  Copyright © 2019 Nasheng Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PlayImage/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  PlayImage
+//
+//  Created by Nasheng Yu on 2019/5/5.
+//  Copyright © 2019 Nasheng Yu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
